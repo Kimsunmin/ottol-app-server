ccd8fd50471a125d489f4f5f8ed3406ea68e3fc5
sunmin <sunmin0406@naver.com>
2023-10-03 18:57:08 +0900

    dto validator 추가

--- src/lotto/dto/select-lotto.dto.ts
+++ src/lotto/dto/select-lotto.dto.ts
@@ -1,8 +1,21 @@
+import { IsNumber} from 'class-validator';
+
 export class SelectLottoDto {
+    @IsNumber()
     drwtNo1: number
+
+    @IsNumber()
     drwtNo2: number
+    
+    @IsNumber()
     drwtNo3: number
+    
+    @IsNumber()
     drwtNo4: number
+    
+    @IsNumber()
     drwtNo5: number
+    
+    @IsNumber()
     drwtNo6: number
 }
