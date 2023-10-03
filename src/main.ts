3957a5a3589915032044cc95ac00ea74f70f7cb9
sunmin <sunmin0406@naver.com>
2023-10-03 18:36:35 +0900

    cors 적용

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { LottoService } from './lotto/lotto.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT', 3000);
   
